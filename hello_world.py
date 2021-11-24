98c03c4243e5f04a0d6091f0c4d7078066cae248
EnesT51 <99040669@mydavinci.nl>
2021-11-24 11:44:46 +0100

    hello world output

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+def wereld(loop):
+    for i in range(loop):
+        print("hello wereld")
+wereld(3)
+
+print("--------------------------")
+
+def wereld(wereld):
+    for i in range(wereld):
+        print("hello wereld")
+wereld(5)
