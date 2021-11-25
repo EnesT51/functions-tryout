fdd1cfbacb4d75b9795cb64d0207bf97ebbb2ab1
EnesT51 <99040669@mydavinci.nl>
2021-11-25 11:33:03 +0100

    berekening opdracht afgerond

---
+++ berekening.py
@@ -0,0 +1,35 @@
+def test1(enes):
+    return 10 + enes
+
+def test2(enes2):
+    return 58 - enes2
+
+def test3(enes3):
+    return 6 * enes3
+
+def test4(enes4): 
+   return 144 / enes4
+
+def test5(enes5):
+    return 12 + enes5
+
+def test6(enes6):
+    return 34 - enes6
+
+
+
+
+print("-----------------------")
+print("10 + 12 =",test1(12))
+print("-----------------------")
+print("58 - 34 =",test2(34))
+print("-----------------------")
+print("6 x 7 =",test3(7))
+print("-----------------------")
+print("144 / 12 =",int(test4(12)))
+print("-----------------------")
+print("12 + 1 =",test5(13))
+print("-----------------------")
+print("34 - 1 =",test6(33))
+print("-----------------------")
+
