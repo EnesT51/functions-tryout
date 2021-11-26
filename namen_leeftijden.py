4baf7a7634edbfc6bffd1695c421c31a42a76c8d
EnesT51 <99040669@mydavinci.nl>
2021-11-26 11:13:47 +0100

    opdracht afgerond

---
+++ namen_leeftijden.py
@@ -0,0 +1,14 @@
+def naam_leeftijd(x,i):
+    print(x,i)
+    
+naam_leeftijd(input("wat is je naam?: "), int (input("hoe oud ben je?: ")))
+
+print("---------------------------------------------------------------------")
+
+def naam():
+    name = input("wat is je naam?: ")
+    leeftijd = int (input("hoe ou ben je?: "))
+    print("ik heet",name,"ik ben",leeftijd,"jaar oud.")
+naam()
+
+
